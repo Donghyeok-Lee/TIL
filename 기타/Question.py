1a9a3eecacee5309ff7b3b038c42812e80c48149
Donghyeok-Lee <lee33843@gmail.com>
2020-01-28 16:04:08 +0900

    버블정렬 설명 추가

--- 기타/Question.py
+++ 기타/Question.py
@@ -3,44 +3,31 @@
 data_list = [[0] * 10] * 10
 
 data_list2 = []
-for i in range(10):
+for _ in range(10):
     data_list2.append([0]*10)
 
-
-
-
-for i in range(10):
-    print(data_list)
+for k in range(10):
+    print(data_list[k])
 
 print('-'*50)
 
-for i in range(10):
-    print(data_list2[i])
+for n in range(10):
+    print(data_list2[n])
 
 print('='*50)
 
-for i in range(3,7):
+for i in range(3, 7):
     for j in range(3, 7):
         data_list[i][j] = 1
         data_list2[i][j] = 1
 
 
 
-
-for i in range(10):
-    print(data_list[i])
+for l in range(10):
+    print(data_list[l])
 
 print('-'*50)
 
-for i in range(10):
-    print(data_list2[i])
+for m in range(10):
+    print(data_list2[m])
 
-
-data_list = [3, 6, 7, 1, 5, 4]
-n = len(data_list) # n:원소의 개수
-
-for i in range(1<<n) : # 1<<n : 부분 집합의 개수
-    for j in range(n): # 원소의 수만큼 비트를 비교함
-        if i & (1<<j) : # i의 j번째 비트가 1이면 j번째 원소 출력
-            print(data_list[j], end=',')
-    print() 
