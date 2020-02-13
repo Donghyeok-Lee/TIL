20ca3a9e7ac4e9ff22d6ee9a71400a375462fbf1
Donghyeok-Lee <lee33843@gmail.com>
2020-02-13 20:02:15 +0900

    20200213 자습 내용 추가

--- 개인 공부/IM 대비 문제 풀이/unknown.py
+++ 개인 공부/IM 대비 문제 풀이/unknown.py
@@ -22,4 +22,53 @@ height_list.sort()
 for height in height_list:
     print(height)
 
+
+
+
+num_list = [
+    [11, 12, 2, 24, 10],
+    [16, 1, 13, 3, 25],
+    [6, 20, 5, 21, 17],
+    [19, 4, 8, 14, 9],
+    [22, 15, 7, 23, 18]
+    ]
+
+numbers = [5, 10, 7, 16, 2, 4, 22, 8, 17, 13, 3, 18, 1, 6, 25, 12, 19, 23, 14, 21, 11, 24, 9, 20, 15]
+
+for num in numbers:
+    for i in range(5):
+        for j in range(5):
+            if num_list[i][j] == num:
+                num_list[i][j] = ''
+
+for i in range(5):
+    print(num_list[i])
+
+
+
+
+N, M = map(int, input().split())
+num_list = list(map(int, input().split()))
+
+max_total = 0
+for i in range(M):
+    max_total += num_list[i]
+
+for i in range(1, N-M+1):
+    temp_total = 0
+    for j in range(M):
+        temp_total += num_list[i+j]
+    if max_total < temp_total:
+        max_total = temp_total
+
+print(max_total)
+
+# 왜 시간초과..
+
+# 처음껄 뺴고, 뒤에걸 더하는 식으로..?
+
+
+
 """
+
+
--- 개인 공부/알고리즘/puzzle_04.py
+++ 개인 공부/알고리즘/puzzle_04.py
@@ -1,3 +1,5 @@
+"""
+
 # N-queen이라서 빨리 해야징
 
 # Step1. 책을 통해 백트래킹 예시 확인하기
@@ -110,3 +112,116 @@ def noConflicts2(board, current):
         if (current - i == abs(board[current] - board[i])):
             return False
     return True
+
+
+def confirm(board, current, qindex, n): # 현재 보드, 현재 퀸을 놓은 위치(열)
+    for i in range(current):
+        if board[i][qindex] == 1:
+            return False
+    k = 1
+    while qindex - k >= 0 and current - k >= 0:
+        if board[current - k][qindex - k] == 1:
+            return False
+        k += 1
+    k = 1
+    while qindex + k < n and current - k < n:  # 대각선 2 검사
+        if board[current - k][qindex + k] == 1:
+            return False
+        k += 1
+    return True  # 행, 대각선 검사에서 return을 안 만날 경우 True를 리턴
+
+#
+def fourQueen(n=4):
+    board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+    for i in range(n):
+        board[0][i] = 1     # 각 행에는 1개의 퀸만 배치함
+        for j in range(n):  # 첫 퀸은 충돌이 날 리 없으니까 다음으로 ㄱㄱ
+            board[1][j] = 1     # 두번째 퀸 배치
+            if confirm(board, 1, j, n): # 기존 퀸과 충돌 확인
+                for k in range(n):  # 충돌 X면 다음 퀸 배치, 반복
+                    board[2][k] = 1
+                    if confirm(board, 2, k, n):
+                        for m in range(n):
+                            board[3][m] = 1
+                            if confirm(board, 3, m, n):
+                                for l in range(5):
+                                    print(board[l])
+                                print()
+                            board[3][m] = 0
+                    board[2][k] = 0
+            board[1][j] = 0
+        board[0][i] = 0
+    return
+
+fourQueen()
+
+def fiveQueen(n=5):
+    board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+    for i in range(n):
+        board[0][i] = 1     # 각 행에는 1개의 퀸만 배치함
+        for j in range(n):  # 첫 퀸은 충돌이 날 리 없으니까 다음으로 ㄱㄱ
+            board[1][j] = 1     # 두번째 퀸 배치
+            if confirm(board, 1, j, n): # 기존 퀸과 충돌 확인
+                for k in range(n):  # 충돌 X면 다음 퀸 배치, 반복
+                    board[2][k] = 1
+                    if confirm(board, 2, k, n):
+                        for m in range(n):
+                            board[3][m] = 1
+                            if confirm(board, 3, m, n):
+                                for p in range(n):
+                                    board[4][p] = 1
+                                    if confirm(board, 4, p, n):
+                                        for l in range(5):
+                                            print(board[l])
+                                        print()
+                                    board[4][p] = 0
+                            board[3][m] = 0
+                    board[2][k] = 0
+            board[1][j] = 0
+        board[0][i] = 0
+    return
+
+fiveQueen()
+
+
+"""
+
+
+
+def confirm(board, current, qindex, n):
+    for i in range(current):                        # 내가 놓은 열에 다른 퀸이 있나요? - 위아래
+        if board[i][qindex] == 1:
+            return False
+    k = 1
+    while qindex - k >= 0 and current - k >= 0:     # 대각선 1에 퀸이 ..(중략)..
+        if board[current - k][qindex - k] == 1:
+            return False
+        k += 1
+    k = 1
+    while qindex + k < n and current - k < n:     # 대각선 2에 퀸이 ..(중략)..
+        if board[current - k][qindex + k] == 1:
+            return False
+        k += 1
+    return True
+
+def queen(n):
+    global cnt
+    if n == N:                          # n == N이면 모든 행에 퀸을 놓았습니다!
+        cnt += 1                        # 숫자를 셉니다.
+        return
+    for i in range(N):
+        board[n][i] = 1                 # n행 에 퀸을 놓는 행위
+        if confirm(board, n, i, N):     # 만약 놓았는데, n행까지 충돌이 없다면
+            queen(n+1)                  # n+1 행에 퀸 놓으러 감
+        board[n][i] = 0                 # 뭐... 어떻게 되면 다시 n행에 놓은 퀸 회수함
+
+T = int(input())
+
+for t in range(T):
+    N = int(input())
+    board = [[0] * N for _ in range(N)]
+    cnt = 0
+    queen(0)                            # 0행부터 퀸을 놓습니다.
+    print('#{0} {1}'.format(t+1, cnt))
