0f2a1a414ff5240bddb70fc32890e8c8a85e7352
Donghyeok-Lee <lee33843@gmail.com>
2020-02-16 20:45:21 +0900

    puzzle 08 추가

--- 개인 공부/알고리즘/puzzle_08.py
+++ 개인 공부/알고리즘/puzzle_08.py
@@ -8,6 +8,9 @@
 # Step3. 완전탐색..?
 # 어차피 5명밖에 안되는데... 부분집합 만들지 뭐..
 
+"""
+# 비트연산자를 이용한 모든 부분집합 만들기!
+
 people = ['A', 'B', 'C', 'D', 'E']
 N = 5
 result_list = []
@@ -29,3 +32,20 @@ for result in result_list:
 
 print(max_list)
 
+"""
+
+# 재귀를 이용한 부분집합..?
+
+def com(n, lst):
+    comb = []
+    for i in range(2**n):
+        num = i
+        temp_list = []
+        for j in range(n):
+            if num % 2:
+                temp_list = [lst[n-1-j]] + temp_list
+            num //= 2
+        comb.append(temp_list)
+    return comb
+
+print(com(5, [1,2,3,4,5]))
